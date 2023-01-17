4a464a459bd0e5a36e3d6b64e2c96453820a7957
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2023-01-16 19:57:38 -0700

    add middleware, cors

--- main.go
+++ main.go
@@ -1,78 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
-	"time"
 
 	"github.com/pluralsight/inventoryservice/product"
 )
-
-var productList []Product
-
-func init() {
-	productsJSON := `[
-		{
-			"productId": 1,
-			"manufacturer": "John-Jenkins",
-			"sku": "p5z343vds",
-			"upc": "9395810000000",
-			"pricePerUnit": "497.45",
-			"quantityOnHand": 9703,
-			"productName": "sticky note"
-		},
-		{
-			"productId": 2,
-			"manufacturer": "Hessel, Schimmel and Feeney",
-			"sku": "i7v300kmx",
-			"upc": "7409790000000",
-			"pricePerUnit": "282.29",
-			"quantityOnHand": 5905,
-			"productName": "leg warmers"
-		},
-		{
-			"productId": 3,
-			"manufacturer": "Swaniawski, Bartoletti and Bruen",
-			"sku": "q0l6597ys7",
-			"upc": "1117840000000",
-			"pricePerUnit": "436.26",
-			"quantityOnHand": 9218,
-			"productName": "lamp shade"
-		}
-	]`
-	err := json.Unmarshal([]byte(productsJSON), &productList)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-func getNextID() int {
-	highestID := -1
-	for _, product := range productList {
-		if highestID < product.ProductID {
-			highestID = product.ProductID
-		}
-	}
-	return highestID + 1
-}
-func findProductByID(productID int) (*Product, int) {
-	for i, product := range productList {
-		if product.ProductID == productID {
-			return &product, i
-		}
-	}
-	return nil, 0
-}
-
-func middlewareHandler(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("before handler; middleware start")
-		start := time.Now()
-		handler.ServeHTTP(w, r)
-		fmt.Printf("middleware finished; %s", time.Since(start))
-	})
-}
 
 const apiBasePath = "/api"
 
